a14bc7f25e0b74ba92570d51f59b80f9d3f367e0
JinHxxxxKim <rlawlsgud217@gmail.com>
2023-08-03 15:31:06 +0900

    kkk

--- src/view/SuccesssView.java
+++ src/view/SuccesssView.java
@@ -1,5 +1,21 @@
 package view;
 
+import dto.IngredientDTO;
+import dto.MenuDTO;
+
+import java.util.List;
+
 public class SuccesssView {
 
+    public static void printSelect(List<IngredientDTO> ingredientDTOS) {
+        for (IngredientDTO ingredientDTO : ingredientDTOS) {
+            System.out.println(ingredientDTO.getIngredientName());
+        }
+    }
+
+    public static void printAllMenu(List<MenuDTO> allMenu) {
+        for (MenuDTO menu : allMenu) {
+            System.out.println(menu.getMenuName());
+        }
+    }
 }
