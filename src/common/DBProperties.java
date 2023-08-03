1cd2deacfa96032b9070f2bb80814de95f88bd96
BullChallenger <am23am45@naver.com>
2023-08-03 16:56:29 +0900

    [FEAT] INIT

--- src/common/DBProperties.java
+++ src/common/DBProperties.java
@@ -3,11 +3,8 @@ package common;
  * DB의 설정 정보들 상수로 관리
  * */
 public interface DBProperties {
-  public static final String DRVIER_NAME="oracle.jdbc.driver.OracleDriver";
+  public static final String DRIVER_NAME ="oracle.jdbc.driver.OracleDriver";
    String URL="jdbc:oracle:thin:@localhost:1521:XE";
-
-   String USER_ID="WONSIK";
-   String USER_PASS="6641";
-
-
+   String USER_ID="Lagoon";
+   String USER_PASS="ROOK";
 }
--- src/dao/MemberOrderDAO.java
+++ src/dao/MemberOrderDAO.java
@@ -15,4 +15,7 @@ public interface MemberOrderDAO<MemberOrderDTO, Long> extends BaseDAO<MemberOrde
     List<MemberOrderDTO> findByMemberId(Long memberId);
 
     List<MemberOrderDTO> findByMenuId(Long menuId);
+
+    // 반환할 시 orderDate 기준으로 정렬하여 최상단 DTO 하나만 반환해야 함
+    MemberOrderDTO findByMenuIdAndMemberId(Long menuId, Long memberId);
 }
