a5ddd60e8cace3c95fdee80653d6b89aef3dea4e
JinHxxxxKim <rlawlsgud217@gmail.com>
2023-08-03 20:22:18 +0900

    asdasdasda

--- src/dao/impl/IngredientDAOImpl.java
+++ src/dao/impl/IngredientDAOImpl.java
@@ -216,7 +216,7 @@ public class IngredientDAOImpl implements IngredientDAO<IngredientDTO, Long> {
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
-        String sql = "SELECT INGREDIENT_ID, INGREDIENT_NAME, STOCK, INGREDIENT_PRICE, INGREDIENT_CALORIE, INGREDIENT_CATEGORY FROM INGREDIENT WHERE INGREDIENT_CATEGORY = ?";
+        String sql = "SELECT INGREDIENT_ID, INGREDIENT_NAME, STOCK, INGREDIENT_PRICE, INGREDIENT_CALORIE, INGREDIENT_CATEGORY FROM INGREDIENT WHERE INGREDIENT_CATEGORY = "+ingredientCategory;
         IngredientDTO theIngredient = new IngredientDTO();
         List<IngredientDTO> ingredientList = new ArrayList<>();
 
